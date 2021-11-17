5949bb362d0f69091a5423619a117c221da8c95a
You Song Hou <yousonghou@gmail.com>
2021-11-17 01:30:32 -0500

    install 'react-signature-canvas' and add it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import SignaturePad from 'react-signature-canvas';
 
 function App() {
+  let sigPad = useRef({});
+
   return (
     <div>
       <h1>Signature Collectable</h1>
+      <SignaturePad penColor='blue' ref={sigPad} />
     </div>
   );
 }
