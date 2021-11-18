14ba17dd30cbc1a1b4eafbcee192bfd5182be81a
You Song Hou <yousonghou@gmail.com>
2021-11-17 23:05:16 -0500

    add buttons for clear, save, and load for signature canvas

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import SignaturePad from 'react-signature-canvas';
 
 function App() {
   let sigPad = useRef({});
+  let signatureData;
+
+  const clear = () => {
+    sigPad.current.clear();
+  }
+
+  const save = () => {
+    signatureData = sigPad.current.toDataURL();
+  }
+
+  const load = () => {
+    sigPad.current.fromDataURL(signatureData);
+  }
 
   return (
     <div>
       <h1>Signature Collectable</h1>
+      <button onClick={clear}>Clear</button>
+      <button onClick={save}>Save</button>
+      <button onClick={load}>Load</button>
       <SignaturePad penColor='blue' ref={sigPad} />
     </div>
   );
