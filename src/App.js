ed697151d7f583c15eb2fceb3f43f3435ea2d8a3
You Song Hou <yousonghou@gmail.com>
2021-11-19 00:12:02 -0500

    add a function to convert Base64 to image

--- src/App.js
+++ src/App.js
@@ -11,10 +11,29 @@ function App() {
 
   const save = () => {
     signatureData = sigPad.current.toDataURL();
+    console.log(signatureData);
+
+    const imageData = convertBase64ToImage(signatureData);
+    console.log(imageData);
   }
 
   const load = () => {
     sigPad.current.fromDataURL(signatureData);
+  }
+
+  const convertBase64ToImage = signatureData => {
+    const filename = "signature.png";
+    let arr = signatureData.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), 
+        n = bstr.length, 
+        u8arr = new Uint8Array(n);
+        
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    
+    return new File([u8arr], filename, {type:mime});
   }
 
   return (
