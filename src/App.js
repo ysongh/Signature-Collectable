9fc7c745e2b388ed0e1d3a0c25847db6d9008c3d
You Song Hou <yousonghou@gmail.com>
2021-11-20 23:20:25 -0500

    install 'react-router-dom' and refactor

--- src/App.js
+++ src/App.js
@@ -1,184 +1,30 @@
-import React, { useState, useRef } from 'react';
-import SignaturePad from 'react-signature-canvas';
+import React, { useState } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import * as htmlToImage from 'html-to-image';
 
 import Navbar from './components/Navbar';
-import SignatureImg from './components/SignatureImg';
-import Board from './components/Board';
-import MintForm from './components/MintForm';
-
-import { SLATEAPIKEY, NFTPORT_APIKEY } from './config';
+import Home from './pages/Home';
 
 function App() {
-  const [sigImgUrl, setSigImgUrl] = useState('https://slate.textile.io/ipfs/bafkreiazivk5dffd6yyslb7higwgbukjx7y4rnyv3igcko5chqcto2m2ba');
-  const [board, setBoard] = useState([]);
   const [account, setAccount] = useState('');
   const [scContract, setSCContract] = useState(null);
 
-  let sigPad = useRef({});
-  let signatureData;
-  let signatureArray = [];
-
-  const clear = () => {
-    sigPad.current.clear();
-  }
-
-  const uploadToIPFS = async () => {
-    signatureData = sigPad.current.toDataURL();
-    console.log(signatureData);
-
-    const imageData = convertBase64ToImage(signatureData);
-    console.log(imageData);
-
-    const url = 'https://uploads.slate.host/api/public';
-
-    let file = imageData;
-    let data = new FormData();
-    data.append("data", file);
-    
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: SLATEAPIKEY, // API key
-      },
-      body: data
-    });
-
-    const json = await response.json();
-    console.log(json);
-    setSigImgUrl(`https://slate.textile.io/ipfs/${json.data.cid}`);
-  }
-
-  const load = () => {
-    sigPad.current.fromDataURL(signatureData);
-  }
-
-  const loadFromContract = async () => {
-    const images = await scContract.methods.getSignatureImages(account).call();
-    setBoard(images);
-    signatureArray = [images];
-  }
-
-  const save = async () => {
-    const tx = await scContract.methods
-      .addImageToCollection(account, board)
-      .send({ from: account });
-
-    console.log(tx);
-  }
-
-  const convertBase64ToImage = signatureData => {
-    const filename = "signature.png";
-    let arr = signatureData.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), 
-        n = bstr.length, 
-        u8arr = new Uint8Array(n);
-        
-    while(n--){
-        u8arr[n] = bstr.charCodeAt(n);
-    }
-    
-    return new File([u8arr], filename, {type:mime});
-  }
-
-  const mint = async (name, description, address) => {
-    const node = document.getElementById('my-node');
-    const dataUrl = await htmlToImage.toPng(node);
-    console.log(dataUrl);
-
-    const imageData = convertBase64ToImage(dataUrl);
-    console.log(imageData);
-
-    const form = new FormData();
-    form.append('file', imageData);
-
-    const options = {
-      method: 'POST',
-      body: form,
-      headers: {
-        "Authorization": NFTPORT_APIKEY,
-      },
-    };
-
-    const response = await fetch("https://api.nftport.xyz/easy_mint?" + new URLSearchParams({
-      chain: 'polygon',
-      name: name,
-      description: description,
-      mint_to_address: address,
-    }), options);
-
-    const json = await response.json();
-    console.log(json);
-    return json.transaction_external_url;
-  }
-
-  const addImageToBoard = () => {
-    console.log("signatureArray", signatureArray)
-    setBoard([...signatureArray, sigImgUrl]);
-  };
-
   return (
     <DndProvider backend={HTML5Backend}>
-      <Navbar
-        account={account}
-        setAccount={setAccount}
-        setSCContract={setSCContract} />
-      <div className='container mt-4'>
-        <div className="row">
-          <div className="col-sm-12 col-md-5">
-            <div className="btn-group btn-group-lg mb-4" role="group" >
-              <button type="button" className="btn btn-outline-primary" onClick={clear}>
-                Clear
-              </button>
-              <button type="button" className="btn btn-outline-primary" onClick={uploadToIPFS}>
-                Upload to IPFS
-              </button>
-              <button type="button" className="btn btn-outline-primary" onClick={load}>
-                Load
-              </button>
-            </div>
-            <p className="text-center h4">Sign Here</p>
-            <div className='signatureContainer'>
-              <SignaturePad
-                penColor='blue'
-                ref={sigPad}
-                canvasProps={{ className: "signaturePad" }} />
-            </div>
-            {sigImgUrl && (
-              <div>
-                <p className="text-center mt-4 h4">Drag and drop to Board</p>
-                <SignatureImg sigImgUrl={sigImgUrl} signatureData={signatureData}/>
-              </div>
-            )}
-          </div>
-
-          <div className="col-sm-12 col-md-7">
-            <div className="btn-group btn-group-lg mb-4" role="group" >
-              <button type="button" className="btn btn-outline-primary" onClick={loadFromContract}>
-                Load from Contract
-              </button>
-              <button type="button" className="btn btn-outline-primary" onClick={save}>
-                Save to Contract
-              </button>
-              <button type="button" className="btn btn-outline-primary"  data-bs-toggle="collapse" data-bs-target="#collapseExample">
-                Mint on Polygon
-              </button>
-            </div>
-            <p className="text-center h4">Your Collection Board</p>
-            <div className="collapse mt-4" id="collapseExample">
-              <MintForm mint={mint} />
-            </div>
-            <div className="card">
-              <Board
-                board={board}
-                addImageToBoard={addImageToBoard} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Navbar
+          account={account}
+          setAccount={setAccount}
+          setSCContract={setSCContract} />
+          <Switch>
+            <Route path="/">
+              <Home
+                account={account}
+                scContract={scContract} />
+            </Route>
+          </Switch>
+      </Router>
     </DndProvider>
   );
 }
