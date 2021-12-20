c49fc0a52585873571cad81b33159b08dda3f23a
You Song Hou <yousonghou@gmail.com>
2021-12-20 01:26:01 -0500

    add a button to navigate the user to How it works page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ import { useHistory } from 'react-router-dom';
 import Alert from '../components/common/Alert';
 
 function Home({ user, uauth, setAccount }) {
-	const router = useHistory();
+  const router = useHistory();
 
-	useEffect(() => {
+  useEffect(() => {
     if (user) router.push('/dashboard');
   }, [user])
 
-	const loginWithUnstoppableDomains = async () => {
+  const loginWithUnstoppableDomains = async () => {
     try {
       const authorization = await uauth.loginWithPopup();
    
@@ -23,15 +23,18 @@ function Home({ user, uauth, setAccount }) {
 
   return (
     <div>
-			<Alert />
+      <Alert />
       <header>
         <div className="container">
           <div className="row py-5">
             <div className="col-12 col-md-6 mb-5">
               <h1 className="mt-4 mb-4">Digital Collection of Signatures </h1>
               <p className="mb-4">Collect signatures from friends, family, or other and store them on IPFS </p>
-              <button className="btn btn-sm btn-lg" onClick={loginWithUnstoppableDomains}>
-								<img src="/unstoppable-default-button.png" alt="Unstoppable Button" width={300} />
+              <button className="btn btn-sm" onClick={loginWithUnstoppableDomains}>
+                <img src="/unstoppable-default-button.png" alt="Unstoppable Button" width={300} />
+              </button>
+              <button className="btn btn-success" onClick={() => router.push('./howitworks')}>
+                How it works
               </button>
             </div>
 
